7f192d1a8d28070dc9179716b1f4f020cacd7414
ArghyadipB <arghyadipbagchi@gmail.com>
2024-10-25 13:24:25 +0530

    Prediction pipeline and application added

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,25 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
+    """Constructs a detailed error message for logging.
+
+    This function extracts the file name, line number, and the error message 
+    from the exception details, and formats them into a single string.
+
+    Args:
+        error (Exception): The exception that was raised.
+        error_detail (sys): The sys module, used to access traceback information.
+
+    Returns:
+        str: A formatted error message containing the script name, line number, 
+             and the original error message.
+
+    Example:
+        try:
+             1 / 0
+         except Exception as e:
+             error_message = error_message_detail(e, sys)
+    """    
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
@@ -12,11 +31,33 @@ def error_message_detail(error,error_detail:sys):
     
 
 class CustomException(Exception):
+    """Custom exception class to handle and log errors.
+
+    This class extends the built-in Exception class to provide additional context
+    for errors that occur in the application. It captures detailed information
+    about the error, including the location in the code where it occurred.
+
+    Attributes:
+        error_message (str): A formatted message detailing the error, including 
+                             the script name and line number.
+    """    
+
     def __init__(self,error_message,error_detail:sys):
+        """Initializes the CustomException with a message and error details.
+
+        Args:
+            error_message (str): The error message provided during the exception.
+            error_detail (sys): The sys module, used to access traceback information.
+        """
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
+        """Returns the error message as a string.
+
+        Returns:
+            str: The formatted error message for the exception.
+        """ 
         return self.error_message
     
 
--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,24 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path, obj):
+def save_object(file_path: str, obj: object):
+    """Saves an object to a specified file path.
+
+    This function serializes the provided object using pickle and 
+    saves it to the specified file. If the directory does not exist, 
+    it will be created.
+
+    Args:
+        file_path (str): The path where the object should be saved, including the filename.
+        obj (object): The object to be saved.
+
+    Raises:
+        CustomException: If an error occurs during saving, 
+                         an exception with the error message and traceback will be raised.
+
+    Example:
+        save_object('artifacts/model.pkl', trained_model)
+    """
     try:
         dir_path = os.path.dirname(file_path)
 
@@ -22,6 +39,30 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    """Evaluates multiple models using the provided training and test data.
+
+    This function trains each model on the training data, performs hyperparameter 
+    tuning using GridSearchCV, and calculates the R² score for both training 
+    and test sets.
+
+    Args:
+        X_train (array-like): Training feature data.
+        y_train (array-like): Training target data.
+        X_test (array-like): Test feature data.
+        y_test (array-like): Test target data.
+        models (dict): A dictionary of models to be evaluated.
+        param (dict): A dictionary of hyperparameters for each model.
+
+    Returns:
+        dict: A dictionary containing the test R² scores for each model.
+
+    Raises:
+        CustomException: If an error occurs during evaluation, 
+                         an exception with the error message and traceback will be raised.
+
+    Example:
+        report = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+    """
     try:
         report = {}
 
@@ -35,19 +76,40 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
-
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    """Loads a serialized object from a specified file path.
+
+    This function deserializes an object from the specified file using pickle.
+
+    Args:
+        file_path (str): The path from where the object should be loaded, including the filename.
+
+    Returns:
+        object: The deserialized object loaded from the file.
+
+    Raises:
+        CustomException: If an error occurs during loading, 
+                         an exception with the error message and traceback will be raised.
+
+    Example:
+        model = load_object('artifacts/model.pkl')
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
